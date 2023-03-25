b6587595d2b760408df2649a08a1d9e23514d011
Barrerse <sebastianbarrera@live.com>
2023-03-25 00:41:21 -0400

    finished back end and intigrated it with front end post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -44,8 +44,31 @@ const CreatePost = () => {
       }
     };
 
-    const handleSubmit = () => {
+    const handleSubmit = async(e) => {
 
+      e.preventDefault();
+
+      if (form.prompt && form.photo){
+        setLoading(true);
+        try {
+          const response = await fetch('http://localhost:8080/api/v1/posts', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(form),
+              });
+              await response.json();
+              navigate('/');
+            } catch (err) {
+              alert(err);
+            } finally {
+              setLoading(false);
+            }
+          } else {
+            alert('Please provide proper prompt and image');
+
+      }
     }
 
     const handleChange = (e) => {
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,5 +8,41 @@ dotenv.config();
 
 const router = express.Router();
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+
+})
+
+router.route('/').get(async(req, res) => {
+    try{
+        const post = await Post.find({});
+        res.status(200).json({sucess: true, data: post});
+    } catch (error) {
+        res.status(500).json({sucess: false, error: error.message});
+    }
+
+});
+
+
+router.route('/').post(async(req, res) => {
+
+try{
+    const { name, promp, photo } = req.body;
+
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post({
+         name, 
+         promp, 
+         photo: photoUrl.secure_url });
+
+    res.status(201).json({sucess: true, data: newPost});
+    } catch (error) {
+        res.status(500).json({sucess: false, error: error.message});
+    }
+
+});
 
 export default router;
